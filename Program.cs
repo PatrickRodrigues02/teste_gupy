aa04626d1e28cd060dec1fba14e1400ba41cebe8
PatrickRodrigues02 <72109090+PatrickRodrigues02@users.noreply.github.com>
2023-04-12 12:43:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inverter_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strNormal = "", strInver = "";
+
+            int[] numAR = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int i = 0; i < numAR.Length; i++)
+            {
+                strNormal += numAR[i];
+            }
+
+            int[] ARinver = new int[strNormal.Length];
+
+            for (int i = 0; i <ARinver.Length; i++)
+            {
+                ARinver[i] = numAR[numAR.Length -1 - i];
+            }
+
+            for (int i = 0; i < ARinver.Length; i++)
+            {
+                strInver += ARinver[i];
+            }
+
+            Console.WriteLine(strNormal);
+            Console.WriteLine(strInver);
+        }
+    }
+}
